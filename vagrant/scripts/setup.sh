444ffd34f12163c02b6b56939b67ec42576b4cab
Ayman Reifoun <git.ayman.reifoun@gmail.com>
2026-03-16 05:45:11 +0000

    done !

--- vagrant/scripts/setup.sh
+++ vagrant/scripts/setup.sh
@@ -1,10 +1,5 @@
 apt update && sudo apt install -y python3-pip git curl ufw
 
-sudo ufw allow ssh
-sudo ufw allow 80
-sudo ufw allow 443
-sudo ufw --force enable
-
 # Install Ansible
 
 sudo apt update && sudo apt install ansible -y
